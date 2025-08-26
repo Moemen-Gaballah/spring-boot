7532ad42bc5c3457d6d8d9e262365290b6eb7578
moemen gaballah <moemengaballa@gmail.com>
2025-08-26 09:14:04 +0300

    added repo and services to fetch data

--- src/main/java/com/springbootmoemen/SoftwareEngineerController.java
+++ src/main/java/com/springbootmoemen/SoftwareEngineerController.java
@@ -10,13 +10,15 @@ import java.util.List;
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
-        return List.of(
-                new SoftwareEngineer(1, "Moemen", "PHP, Laravel"),
-                new SoftwareEngineer(2, "Eslam", "js, node, react"),
-                new SoftwareEngineer(3, "Hamza", "java, spring, spring boot")
-        );
+        return softwareEngineerService.getAllSoftwareEngineers();
     }
 
     ;
